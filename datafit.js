0f8873e3d45c39f840e9649085aeff8605e4a070
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-08-30 17:17:40 +0900

    Add files via upload

--- datafit.js
+++ datafit.js
@@ -1,4 +1,8 @@
 var firebase = require("firebase/app");
+const express = require('express')
+const app = express()
+var path = require('path');
+const { connected } = require("process");
 
 // Add the Firebase products that you want to use
 require("firebase/auth");
@@ -22,43 +26,171 @@ var usersinformations = new Map()
 
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
-let doc = db.collection("name").get()
-    .then((snapshot) => {
-        //이름 데이터 가져오기.
-        snapshot.forEach((doc) => {
-            name = doc.data()['username']
-            delname = doc.data()['deletename']
-            console.log(1, name);
-            console.log(2, delname);
+
+
+//회원 목록을 보여줄 홈페이지.
+var userlist = `<table>`
+app.get('/', (req, res) => {
+    let doc = db.collection("name").get()
+        .then((snapshot) => {
+            //이름 데이터 가져오기.
+            snapshot.forEach((doc) => {
+                name = doc.data()['username']
+                delname = doc.data()['deletename']
+                console.log(1, name);
+                console.log(2, delname);
+            })
+            console.log(name);
+            for (i = 0; i < name.length; i++) {
+                userlist += `<tr>
+                <th><a href="/userinformation/${name[i]}">${name[i]}</th>
+                </tr>`
+            }
+            userlist += `</table>`
+            var html = `
+            <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    ${userlist}
+</body>
+</html>`
+            res.send(html);
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err)
+        })
+
+})
+
+//회원 정보 가져오기.
+app.get('/userinformation/:username', (req, res) => {
+    var filteredId = path.parse(req.params.username).base;
+    console.log(filteredId)
+    db.collection(`${filteredId}`).doc("userinformation").get()
+        .then((it) => {
+            var userdata = it.data()
+            var fat = userdata['fat']
+            var fatlist = ``
+            var muscle = userdata['muscle']
+            var musclelist = ``
+            for (i = 0; i < muscle.length; i++) {
+                fatlist += `<th>${Object.keys(fat[i])}</th>
+                        <th>${Object.values(fat[i])}</th>`
+                musclelist += `<th>${Object.keys(muscle[i])}</th>
+                        <th>${Object.values(muscle[i])}</th>`
+            }
+            console.log(fatlist)
+            var userinfotable = `
+        <table>
+            <tr>
+                <th>${userdata['name']}</th>
+                <th>${userdata['age']}</th>
+                <th>${userdata['sex']}</th>
+                <th>${userdata['weight']}</th>
+                <th>${userdata['height']}</th>
+            </tr>
+            <tr>
+                <th>muscle</th>
+                ${musclelist}
+            </tr>
+            <tr>
+                <th>fat</th>
+                ${fatlist}
+            </tr>
+        </table>
+        `
+            var html = `
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+<a href="/userinformation/userexercisedata/${filteredId}">limyohan</a><br>
+${userinfotable}
+</body>
+</html>`
+            res.send(html);
         });
-        console.log(name);
-
-        //회원 기본 정보 가져오기.
-        for (var i = 0; i < name.length; i++) {
-            db.collection(`${name[i]}`).doc("userinformation").get()
-                .then((userinfo) => {
-                    usersinformations.set(userinfo.data()['name'], userinfo.data())
-                    // console.log(usersinformations);
-                })
-        }
-
-
-    })
-    .catch((err) => {
-        console.log('Error getting documents', err)
-    })
+})
 
 
 //회원 운동 데이터 가져오기.
-var uiname = 'kimhyungsuk'
-var date = '2020.8.25'
+app.get('/userinformation/userexercisedata/:username', (req, res) => {
+    var filteredId = path.parse(req.params.username).base;
+    console.log(filteredId)
+    var date = []
+    var datehtml = `<ul>`
+    db.collection(`${filteredId}/userinformation/exerciseinformation`).get()
+        .then((value, err) => {
+            value.forEach((doc) => {
+                date.push(doc.data()['thisdate'][0])
+            });
+            console.log(date);
+            for (i = 0; i < date.length; i++) {
+                datehtml += `<li><a href='/userinformation/userexercisedata/${filteredId}/${date[i]}'>${date[i]}</li>`
+            }
+            datehtml += `</ul>`
+            var html = `
+            <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    ${datehtml}
+</body>
+</html>`
+            res.send(html)
+        });
+});
 
-ExerciseDataGet=db.collection(`${uiname}`).doc(`userinformation/exerciseinformation/${date}`).get()
-.then((exerciseinformation)=>{
-    console.log(exerciseinformation.data())
-})
-.catch((err) => {
-    console.log('Error getting documents', err)
-})
+app.get('/userinformation/userexercisedata/:username/:date', (req, res) => {
+    var filteredId = path.parse(req.params.username).base;
+    var date = path.parse(req.params.date).base;
+    var exerciseinfo = `<table>`
+    db.collection(`${filteredId}`).doc(`userinformation/exerciseinformation/${date}`).get()
+        .then((it) => {
+            var key = Object.keys(it.data())
+            var value = Object.values(it.data())
+            for (i = 0; i < key.length; i++) {
+                if (value[i].length == 0) {
 
-    
+                } else {
+                    exerciseinfo += `
+                <tr>
+                    <th>${key[i]}</th>
+                    <th>${value[i]}</th>
+                </tr>`
+                }
+
+            }
+            exerciseinfo += `</table>`
+            var html = `
+            <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    ${exerciseinfo}
+</body>
+</html>`
+            res.send(html)
+        });
+});
+
+app.listen(300, () => {
+    console.log("welecome");
+});
